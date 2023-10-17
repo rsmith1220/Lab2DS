a03bca6076720742b86d51951fbdbbf4071822ae
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2023-10-16 21:43:36 -0600

    grafica 3 terminada

--- graph2.py
+++ graph2.py
@@ -7,6 +7,8 @@ import holoviews as hv
 data = [25.49, 26.38, 22.26]
 labels = ["Regular", "Super", "Diesel"]
 
+pn.config.theme = 'dark'
+
 # Read the CSV data
 def read_csv_data(filepath):
     df = pd.read_csv(filepath, index_col=0)
@@ -30,7 +32,8 @@ def create_plot(regular, super_fuel, diesel):
     filtered_df = df[df['Tipo de gasolina'].isin(
         [label for label, checkbox in zip(labels, [regular, super_fuel, diesel]) if checkbox]
     )]
-    bar_plot = filtered_df.hvplot.bar(x='Tipo de gasolina', y='Amount', alpha=0.6, label='Promedio de gasolina por tipo', width=800)
+    colors = {'Regular': '#abdfff', 'Super': '#0091e4', 'Diesel': '#00224f'}  # Blue tones
+    bar_plot = filtered_df.hvplot.bar(x='Tipo de gasolina', y='Amount', color=filtered_df['Tipo de gasolina'].map(colors), alpha=0.6, label='Promedio de gasolina por tipo', width=700)
     vline = hv.VLine(filtered_df['Amount'].mean()).opts(color='red', line_width=2)
     average_of_data = filtered_df['Amount'].mean()
     label = hv.Text(1, average_of_data + 1, f'Promedio total: {average_of_data:.2f}').opts(text_font_size='10pt', text_color='red')
@@ -43,24 +46,48 @@ def create_plot2(regular, super_fuel, diesel):
     if regular:
         df = read_csv_data('data/gasregularpormes.csv')
         monthly_averages = df.mean(axis=1, skipna=True)
-        plots.append(hv.Curve(monthly_averages, label='Regular').opts(color='blue'))
+        plots.append(hv.Curve(monthly_averages, label='Regular').opts(color='#abdfff'))  # Dark blue
         
     if super_fuel:
         df = read_csv_data('data/gassuperpormes.csv')
         monthly_averages = df.mean(axis=1, skipna=True)
-        plots.append(hv.Curve(monthly_averages, label='Super').opts(color='green'))
+        plots.append(hv.Curve(monthly_averages, label='Super').opts(color='#0091e4'))  # Light blue
         
     if diesel:
         df = read_csv_data('data/dieselpormes.csv')
         monthly_averages = df.mean(axis=1, skipna=True)
-        plots.append(hv.Curve(monthly_averages, label='Diesel').opts(color='red'))
+        plots.append(hv.Curve(monthly_averages, label='Diesel').opts(color='#00224f'))  # Lighter blue
         
     if not plots:
         return hv.Curve([])  # Empty plot if none are selected
     
-    overlay = hv.Overlay(plots).opts(width=800, xlabel='Mes', ylabel='Precio', title='Promedios mensuales')
+    overlay = hv.Overlay(plots).opts(width=1200, xlabel='Mes', ylabel='Precio', title='Promedios mensuales')
     return overlay
 
+@pn.depends(regular_checkbox.param.value, super_checkbox.param.value, diesel_checkbox.param.value)
+def create_plot3(regular, super_fuel, diesel):
+    plots = []
+    
+    if regular:
+        df = read_csv_data('data/gasregularpormes.csv')
+        monthly_averages = df.mean(axis=1, skipna=True)
+        plots.append(hv.Bars(monthly_averages, label='Regular').opts(color='#abdfff'))
+        
+    if super_fuel:
+        df = read_csv_data('data/gassuperpormes.csv')
+        monthly_averages = df.mean(axis=1, skipna=True)
+        plots.append(hv.Bars(monthly_averages, label='Super').opts(color='#0091e4'))
+        
+    if diesel:
+        df = read_csv_data('data/dieselpormes.csv')
+        monthly_averages = df.mean(axis=1, skipna=True)
+        plots.append(hv.Bars(monthly_averages, label='Diesel').opts(color='#00224f'))
+        
+    if not plots:
+        return hv.Bars([])  # Empty plot if none are selected
+    
+    overlay = hv.Overlay(plots).opts(width=1200, xlabel='Month', ylabel='Average Value', title='Monthly Averages')
+    return overlay
 
 # Options panel
 options_panel = pn.WidgetBox(
@@ -73,13 +100,15 @@ options_panel = pn.WidgetBox(
 # Bind the create_plot and create_plot2 functions to the checkbox widgets
 bound_plot1 = pn.bind(create_plot, regular=regular_checkbox, super_fuel=super_checkbox, diesel=diesel_checkbox)
 bound_plot2 = pn.bind(create_plot2, regular=regular_checkbox, super_fuel=super_checkbox, diesel=diesel_checkbox)
+bound_plot3 = pn.bind(create_plot3, regular=regular_checkbox, super_fuel=super_checkbox, diesel=diesel_checkbox)
 
 # Create two dynamic maps to update the plots based on the checkbox values
 dynamic_plot1 = hv.DynamicMap(bound_plot1)
 dynamic_plot2 = hv.DynamicMap(bound_plot2)
+dynamic_plot3 = hv.DynamicMap(bound_plot3)
 
 # Combine the two dynamic plots in a column layout
-combined_plot = pn.Column(dynamic_plot1, dynamic_plot2)
+combined_plot = pn.Column(dynamic_plot1, dynamic_plot2, dynamic_plot3)
 
 # Create a Panel app with the options panel on the left and the combined plots on the right
 app = pn.Row(options_panel, combined_plot)
